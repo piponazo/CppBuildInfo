40bae9f2ac9979ed0d0cbd211d3e6197033fe620
Luis Diaz Mas <piponazo@gmail.com>
2016-08-13 13:47:32 +0200

    Cmd and Gui applications in different directories. Initial skeleton for gui app

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -1,5 +1,6 @@
 find_program(QT_QMAKE_EXECUTABLE NAMES qmake-qt5)
 find_package(Qt5Core REQUIRED)
+find_package(Qt5Widgets REQUIRED)
 
 if (${Qt5Core_VERSION} VERSION_LESS 5.7)
     message(FATAL_ERROR "CppBuildInfo requires at least Qt 5.7. Version found: ${Qt5Core_VERSION}")
