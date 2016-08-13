65e912b14b371ddd299f26ae869e984af65efad8
Luis Diaz Mas <piponazo@gmail.com>
2016-08-13 16:08:33 +0200

    MainWindow add an action for opening the input file and use DataParser class

--- apps/gui/MainWindow.cpp
+++ apps/gui/MainWindow.cpp
@@ -1,6 +1,12 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
+#include "DataParser.h"
+
+#include <QDir>
+#include <QFileDialog>
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +17,20 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionOpenFile_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QDir::homePath(), tr("Text file (*.txt)"));
+    if (!fileName.isEmpty()) {
+        DataParser parser(fileName);
+        auto data = parser.getData();
+
+        QHashIterator<QString, int> i(data);
+        while (i.hasNext()) {
+            i.next();
+            qDebug() << i.key() << ": " << i.value();
+        }
+    }
+
 }
