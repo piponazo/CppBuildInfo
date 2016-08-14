69325d508fd1ea9d5509d694244e3a0cd361b62d
Luis Díaz Más <piponazo@gmail.com>
2016-08-14 17:02:53 +0200

    Installing cbi app and setting RPATH correctly

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -6,3 +6,5 @@ find_package(Qt5Charts REQUIRED)
 if (${Qt5Core_VERSION} VERSION_LESS 5.7)
     message(FATAL_ERROR "CppBuildInfo requires at least Qt 5.7. Version found: ${Qt5Core_VERSION}")
 endif ()
+
+set(CMAKE_INSTALL_RPATH ${CMAKE_PREFIX_PATH}/lib)
