3e3b4f4aa271747f6144945710fe6aa654a81589
Luis Díaz Más <Luis Daz Ms>
2016-08-31 18:03:14 +0200

    Depend on Qt LTS

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -1,10 +1,9 @@
 find_program(QT_QMAKE_EXECUTABLE NAMES qmake-qt5)
 find_package(Qt5Core REQUIRED)
 find_package(Qt5Widgets REQUIRED)
-find_package(Qt5Charts REQUIRED)
 
-if (${Qt5Core_VERSION} VERSION_LESS 5.7)
-    message(FATAL_ERROR "CppBuildInfo requires at least Qt 5.7. Version found: ${Qt5Core_VERSION}")
+if (${Qt5Core_VERSION} VERSION_LESS 5.6)
+    message(FATAL_ERROR "CppBuildInfo requires at least Qt 5.6. Version found: ${Qt5Core_VERSION}")
 endif ()
 
 if (COMPILE_TESTS)
