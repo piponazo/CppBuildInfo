382529dd0cc1149b5a4907a7b02b6183df553c78
Luis Díaz Más <piponazo@gmail.com>
2016-09-04 16:39:29 +0200

    005 - Fix recent files on Windows

--- apps/guiGraphicsView/main.cpp
+++ apps/guiGraphicsView/main.cpp
@@ -4,6 +4,9 @@
 int main (int argc, char **argv)
 {
     QApplication app(argc, argv);
+	QCoreApplication::setOrganizationName("FreeSoftware");
+	QCoreApplication::setApplicationName("CppBuildInfo");
+
     MainWindow window;
     window.show();
     return app.exec();
